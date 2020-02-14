b999d594ef11209a40cbbdc396a848b7c66fd7e1
h-takauma <h-takauma@i-enter.co.jp>
2020-02-14 14:58:49 +0900

    first commit

---
+++ GASPost.js
@@ -0,0 +1,42 @@
+var request = require('request');
+
+var headers = {
+  'Content-Type':'application/json'
+}
+
+class GASPost {
+    constructor() {
+    }
+
+    post(uuid) {
+        //オプションを定義
+        let jsonData =
+        {
+            "uuid": uuid,
+        };
+        let payload = JSON.stringify(jsonData);
+
+        // request options
+        let options = {
+            url: process.env.SPREADSHEET_URL,
+            method: 'POST',
+            headers: headers,
+            form: payload
+        };
+
+        return new Promise(function(resolve, reject) {
+            request(options, function(error, response, body){
+                if (!error && response.statusCode == 200) {
+                    resolve(true);
+
+                } else {
+                    console.log('error: '+ response.statusCode + body);
+                    reject(error);
+                }
+            });
+        });
+
+    }
+
+}
+module.exports = GASPost;
---
+++ index.js
@@ -0,0 +1,54 @@
+"use strict";
+
+const line = require("@line/bot-sdk");
+const express = require("express");
+require('dotenv').config();
+const GAS = require('./GASPost');
+
+const config = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET
+};
+
+const client = new line.Client(config);
+const gas = new GAS();
+
+const app = express();
+app.use('/static', express.static('static'));
+
+app.post("/callback", line.middleware(config), (req, res) => {
+  Promise.all(req.body.events.map(handleEvent))
+    .then(result => res.json(result))
+    .catch(err => {
+      console.error(err);
+      res.status(500).end();
+    });
+});
+
+async function handleEvent(event, session) {
+  let echo = [];
+  if (event.type === 'beacon') {
+    if (event.beacon.type === 'enter') {
+        await gas.post(event.source.userId).then(function (ret) {
+            if (ret) {
+                console.log('GAS送信OK');
+            }
+        });
+        echo = { 'type': 'text', 'text': 'ビーコン受信しました！' };
+        return client.replyMessage(event.replyToken, echo);
+    }
+    return;
+  } else if (event.type === 'follow') {
+    return;
+  } else {
+    echo = { 'type': 'text', 'text': '申し訳ありませんが、お返事できません。' }; 
+  }
+
+  // use reply API
+  return client.replyMessage(event.replyToken, echo);
+}
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`listening on ${port}!`);
+});
